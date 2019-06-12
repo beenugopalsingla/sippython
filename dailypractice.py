735110d05b585656fa718627d48da1281e7ad7d7
beenu gopal singla <beenusingla1997@gmail.com>
2019-06-12 12:56:15 +0530

    12 june daily practice

--- dailypractice.py
+++ dailypractice.py
@@ -8,3 +8,12 @@ Created on Wed Jun 12 11:30:15 2019
 
 list1 = [1,2,3,4,5]
 list1
+
+list2 = ['a','c','d','e']
+list2
+
+tuple1 = (1,2, 'a', 'b')
+tuple1
+
+dict1 = {1:'Ramesh', 2:'Suresh', 3:'Priyanka'}
+dict1
