8adf6f21a5bf55b7ef97c2b889f584bff316a45a
beenu gopal singla <beenusingla1997@gmail.com>
2019-06-14 10:03:22 +0530

    14 june daily practice

---
+++ dailypractice2.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 11:30:15 2019
+
+@author: beenugopalsingla sip
+
+"""
+
+list1 = [1,2,3,4,5]
+list1
+
+#%%
+list2 = ['a','c','d','e']
+list2
+
+#%%
+tuple1 = (1,2, 'a', 'b')
+tuple1
+
+#%%
+dict1 = {1:'Ramesh', 2:'Suresh', 3:'Priyanka'}
+dict1
+
+#%%
+set1 = set(['india','pakistan','england','australia','india'])
+set1
+
+#%%
+str1 = 'Python Programming'
+
+#%%
+list1
+for i in list1:
+    print(i)
+tuple1
+for i in tuple1:
+    print(i)
+for i in range(1, 10, 2):
+    print(i, end=' ')
+    
+#%%
+tupleFZ1 = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+
+#%%
+frozenset1 = frozenset(tupleFZ1)
+frozenset1
+
+#%%
+dict1
+frozenset2 = frozenset(dict1)
+
+frozenset2
+
+
+#%%
+
+
+
+
+
+
+
+
+
---
+++ untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 11:21:58 2019
+
+@author: hp laptop
+"""
+#list - ordered collection of items, mutable
+list1 = [1,2,3,4,5]
+list1
