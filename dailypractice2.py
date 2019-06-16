0d5ca1870002d374a8c60b8728a51335b27f8ad3
beenu gopal singla <beenusingla1997@gmail.com>
2019-06-16 11:53:07 +0530

    16 june

--- dailypractice2.py
+++ dailypractice2.py
@@ -9,24 +9,41 @@ Created on Wed Jun 12 11:30:15 2019
 list1 = [1,2,3,4,5]
 list1
 
+list3 = [3,5,65,43]
+list3
+
 #%%
 list2 = ['a','c','d','e']
 list2
+
+list4 = ['g','k','t']
+list4
 
 #%%
 tuple1 = (1,2, 'a', 'b')
 tuple1
 
+tuple2 = (56,65, 'e', 'u')
+tuple2
+
 #%%
 dict1 = {1:'Ramesh', 2:'Suresh', 3:'Priyanka'}
 dict1
 
-#%%
-set1 = set(['india','pakistan','england','australia','india'])
-set1
+dict2 = {5:'mayank', 6:'shashank'}
+dict2
 
 #%%
+set1 = (['india','pakistan','england','australia','india'])
+set1
+
+
+set2 = (['food','drinks'])
+set2
+#%%
 str1 = 'Python Programming'
+
+str2 = 'Python Programming'
 
 #%%
 list1
